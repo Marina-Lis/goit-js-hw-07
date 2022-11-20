99e3c454318706e12372e0f8360f56b130c24791
Marina-Lis <105066555+Marina-Lis@users.noreply.github.com>
2022-11-21 00:42:35 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,24 +1,18 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
-
 const galeryPalette = document.querySelector(".gallery");
 
-{/* <a class="gallery__item" href="large-image.jpg">
-  <img class="gallery__image" src="small-image.jpg" alt="Image description" />
-</a> */}
-
 galleryItems.forEach((value) => {
-    const newEl = `<a class="gallery__item" href="${value.original}">
-                              <img class="gallery__image" src="${value.preview}" alt="${value.description}" />
-                          </a>`;
-    galeryPalette.insertAdjacentHTML("beforeend", newEl);
-  });
+  const newEl = `<a class="gallery__item" href="${value.original}">
+                      <img class="gallery__image" src="${value.preview}" alt="${value.description}" />
+                 </a>`;
+  galeryPalette.insertAdjacentHTML("beforeend", newEl);
+});
 
-  let gallery = new SimpleLightbox('.gallery a');
-  gallery.on('show.simplelightbox', function () {
-    animationSpeed,
-    captionPosition
-  });
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
